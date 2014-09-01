05b5fa72627553c2094fa92c0ccea1be5a1adf14
Frank Gutierrez <dearfrankg@gmail.com>
2014-09-01 08:49:55 -0700

    feature: app.hbs is now a template for index.html

--- gulp/tasks/markup.js
+++ gulp/tasks/markup.js
@@ -1,6 +1,38 @@
 var gulp = require('gulp');
+var handlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
+var pkg = require('../../package.json');
 
 gulp.task('markup', function() {
-  return gulp.src('app/htdocs/**')
-    .pipe(gulp.dest('build'));
+  var templateData = {
+       title: pkg.name
+  },
+  options = {
+      ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
+      // partials : {
+      //     footer : '<footer>the end</footer>'
+      // },
+      // batch : ['./src/partials'],
+      helpers : {
+          capitals : function(str){
+            return str.toUpperCase();
+          },
+          camelCase : function (str) {
+            var words = str.split('-');
+            words.forEach(function (word, index, array) {
+              var newWord = word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();
+              array[index] = newWord;
+            });
+            var result = words.join('');
+            return result;
+          }
+
+      }
+  };
+
+  return gulp.src('app/app.hbs')
+      .pipe(handlebars(templateData, options))
+      .pipe(rename('index.html'))
+      .pipe(gulp.dest('./build/'));
+
 });
